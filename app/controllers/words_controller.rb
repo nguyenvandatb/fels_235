f423c5872cbd319c74a63909d8d2225b0932ef93
Hoang Duc Trung <hoangtrungmta94@gmail.com>
2017-02-16 08:27:36 +0700

    Filter words list

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,17 @@
 class WordsController < ApplicationController
   before_action :logged_in_user, only: :index
   def index
-    @words = Word.includes(:answers).paginate page: params[:page],
-      per_page: Settings.per_page_word
+    @categories = Category.all
+    if params[:category_id].present? || params[:q].present? ||
+        params[:action_met].present?
+      @words = Word.includes(:answers)
+                   .filter_category(params[:category_id])
+                   .search(params[:q])
+                   .send(params[:action_met], current_user.id)
+                   .paginate page: params[:page], per_page: Settings.per_page_word
+    else
+      @words = Word.includes(:answers).paginate page: params[:page],
+        per_page: Settings.per_page_word
+    end
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,15 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :results
   has_many :answers
+
+  scope :filter_category, ->category_id{where category_id: category_id if category_id.present?}
+  scope :search, -> q{where "content LIKE ?", "%#{q}%"}
+  scope :learned, ->user_id{where "id IN (SELECT word_id FROM answers WHERE is_correct = true
+   AND id IN (SELECT word_id FROM results WHERE lesson_id IN
+    (SELECT id FROM lessons WHERE user_id = #{user_id})))"}
+  scope :not_learned, ->user_id{where "id NOT IN (SELECT word_id FROM answers WHERE is_correct = true
+   AND id IN (SELECT word_id FROM results WHERE lesson_id IN
+    (SELECT id FROM lessons WHERE user_id = #{user_id})))"}
+  scope :all_words, ->user_id{}
+  scope :alphabet, ->user_id{order("content")}
 end
